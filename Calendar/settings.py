ef56f2dbf44246c9e7a574f45e9740a122232cc5
Ace <saadnaseer63@yahoo.de>
2019-02-28 18:17:57 +0100

    Updates for some minor updates

--- Calendar/settings.py
+++ Calendar/settings.py
@@ -3,10 +3,12 @@
 ical_urls = [
 "https://calendar.google.com/calendar/ical/en.usa%23holiday%40group.v.calendar.google.com/public/basic.ics"
 ]
-events_max_range = "60" # How far (in days) should the events from your iCalendar be fetched?
-api_key = "" #your owm-api-key
-location = "Stuttgart, DE" #your location
-week_starts_on = "Monday" #Sunday or Monday
-language = "en" #en=english, de=german, ru=russian
-units = "metric" #metric or imperial?
-hours = "24" #12 or 24?
+
+events_max_range = "60"
+api_key = ""
+location = "Julich, DE"
+week_starts_on = "Monday"
+display_colours = "bwr"
+language = "en"
+units = "metric"
+hours = "24"
